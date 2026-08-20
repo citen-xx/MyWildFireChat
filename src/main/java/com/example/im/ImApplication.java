05e67b61d54029340c3d0497a5140b8a8ee0529c
Yihua Liu <2659625710@qq.com>
2026-08-20 20:36:30 +0800

    test3

--- src/main/java/com/example/im/ImApplication.java
+++ src/main/java/com/example/im/ImApplication.java
@@ -19,7 +19,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 })
 public class ImApplication {
 
-    public static void main(String[] args) {
+         public static void main(String[] args) {
         SpringApplication.run(ImApplication.class, args);
     }
 }
