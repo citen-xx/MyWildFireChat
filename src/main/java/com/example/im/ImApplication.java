ff59c948ba548d5f9ee211e6510d0c6d7e12a578
Yihua Liu <2659625710@qq.com>
2026-08-20 20:42:36 +0800

    Revert "test3"
    This reverts commit 05e67b61

--- src/main/java/com/example/im/ImApplication.java
+++ src/main/java/com/example/im/ImApplication.java
@@ -18,8 +18,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
         RabbitMqProperties.class
 })
 public class ImApplication {
-//??
-         public static void main(String[] args) {
+
+    public static void main(String[] args) {
         SpringApplication.run(ImApplication.class, args);
     }
 }
